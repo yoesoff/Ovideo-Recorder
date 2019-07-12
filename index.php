3252c3ff1c16557aa8e1cc9eccc1f0e40112ff82
Moh. Yusuf x220 <mhyusufibrahim@gmail.com>
2019-07-12 17:36:43 +0700

    Tersimpan di local storage

--- index.php
+++ index.php
@@ -19,6 +19,8 @@
     <!--When recording either audio/video, video-only, animated GIF or a single image, include a video element -->
     <video id="myVideo" playsinline class="video-js vjs-default-skin"></video>
 
+    <ul id="filelist"></ul>
+
   </body>
 
   <script src="js/video.min.js"></script>
@@ -27,6 +29,109 @@
   <script src="js/videojs.record.min.js"></script>
   <script src="js/videojs.record.ts-ebml.min.js"></script>
   <script>
+
+  function createTime() {
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    return date+'_'+time;
+  }
+
+    var tmpBlob = null;
+
+    // filesystem access
+    // https://www.html5rocks.com/en/tutorials/file/filesystem/
+    function createVideo(fs) {
+      console.log('Opened file system: ' + fs.name);
+
+      fs.root.getFile('vid_'+createTime()+'.webm', {create: true, exclusive: false}, function(fileEntry) {
+
+        // Create a FileWriter object for our FileEntry (log.txt).
+        fileEntry.createWriter(function(fileWriter) {
+
+          fileWriter.onwriteend = function(e) {
+            console.log('Write completed.');
+          };
+
+          fileWriter.onerror = function(e) {
+            console.log('Write failed: ' + e.toString());
+          };
+
+          // Create a new Blob and write it to log.txt.
+          // var blob = new Blob(['Lorem Ipsum'], {type: 'text/plain'});
+
+          fileWriter.write(tmpBlob);
+
+        }, errorHandler);
+
+      }, errorHandler);
+    }
+
+    function toArray(list) {
+      return Array.prototype.slice.call(list || [], 0);
+    }
+
+    function listResults(entries, fs) {
+      // Document fragments can improve performance since they're only appended
+      // to the DOM once. Only one browser reflow occurs.
+      var fragment = document.createDocumentFragment();
+
+      entries.forEach(function(entry, i) {
+        var li = document.createElement('li');
+        var linkObj = null;
+
+        li.innerHTML = ['<a id="'+entry.name.replace(/[^a-z0-9]/gi,'')+'" href rel="stylesheet" download="'+entry.name+'">', entry.name, '</a>'].join('');
+        fragment.appendChild(li);
+
+        // get video by name and set to download link
+        fs.root.getFile(entry.name, {}, function(entry) {
+          entry.file(function(file) {
+             console.log(file);
+
+             var link = document.getElementById(entry.name.replace(/[^a-z0-9]/gi,''));
+             link.href = window.URL.createObjectURL(file);
+          }, errorHandler);
+
+        }, errorHandler);
+        // end get
+
+
+
+      });
+
+      document.querySelector('#filelist').appendChild(fragment);
+    }
+
+    function readVideos(fs) {
+      var dirReader = fs.root.createReader();
+      var entries = [];
+
+      // Call the reader.readEntries() until no more results are returned.
+      var readEntries = function() {
+         dirReader.readEntries (function(results) {
+          if (!results.length) {
+            listResults(entries.sort(), fs);
+          } else {
+            entries = entries.concat(toArray(results));
+            readEntries();
+          }
+        }, errorHandler);
+      };
+
+      readEntries(); // Start reading dirs.
+    }
+
+    function errorHandler(fs) {
+      console.log('Opened file system: ' + fs.name);
+      //alert("Ups, please don't use incognito mode!");
+    }
+
+    window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+    window.requestFileSystem(window.TEMPORARY, 50*1024*1024 /*5MB*/, readVideos, errorHandler);
+    // end of filesystem 
+
+
+    // start video stuff
     var player = videojs('myVideo', {
     // video.js options
     controls: true,
@@ -51,9 +156,9 @@
         console.log('Recording....!');
     });
 
-     // user completed recording and stream is available
+    // user completed recording and stream is available
     player.on('finishRecord', function() {
-      #player.record().saveAs({'video': 'my-video-file-name.webm'});
+      //player.record().saveAs({'video': 'my-video-file-name.webm'});
     });
 
     // converter ready and stream is available
@@ -61,7 +166,11 @@
       // the convertedData object contains the converted data that
       // can be downloaded by the user, stored on server etc.
       // console.log('finished converting: ', player.convertedData);
-      player.record().saveAs({'video': 'my-video-file-converted.webm'}); // pure
+      // player.record().saveAs({'video': 'my-video-file-converted.webm'});
+
+      // Request access to save file.
+      tmpBlob = player.convertedData;
+      window.requestFileSystem(window.TEMPORARY, 50*1024*1024 /*50MB*/, createVideo, errorHandler);
     });
 
   </script>
